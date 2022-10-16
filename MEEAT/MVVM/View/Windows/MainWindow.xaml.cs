9448a7036d2de2e28226c6b50322c9b9f2cbff93
Emanuel dos Santos <ernelemanuel10@hotmail.com>
2022-10-17 00:50:52 +0100

    Adicionar arquivos de projeto.

---
+++ MEEAT/MVVM/View/Windows/MainWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System.Windows;
+using System.Windows.Input;
+
+namespace MEEAT.MVVM.View.Windows
+{
+    /// <summary>
+    /// Lógica interna para MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Border_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.ChangedButton == MouseButton.Left)
+            {
+                this.DragMove();
+            }
+        }
+
+        private bool IsMaximized = false;
+        private void Border_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.ClickCount == 2)
+            {
+                if (IsMaximized)
+                {
+                    this.WindowState = WindowState.Normal;
+                    IsMaximized = false;
+                }
+                else
+                {
+                    this.WindowState = WindowState.Maximized;
+
+                    IsMaximized = true;
+                }
+            }
+        }
+    }
+}
